37fe3bb4fbc25db0350eff4710023b5a6f26aecd
trong anh nguyen thanh <tronganhnguyen222@gmail.com>
2020-11-04 00:35:11 +0700

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,7 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "270px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-}
-
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
