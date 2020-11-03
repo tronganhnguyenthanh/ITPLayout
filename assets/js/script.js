85d0dcdb7c81718480d9f87952882b8c5ec8b48a
Trong Anh <tronganhnguyen222@gmail.com>
2020-11-03 16:54:55 +0700

    tao popup layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,15 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "270px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-/* When the user clicks on the button, 
-   toggle between hiding and showing the dropdown content */
-   function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-  
-  var modal = document.getElementById('id01');
+}
